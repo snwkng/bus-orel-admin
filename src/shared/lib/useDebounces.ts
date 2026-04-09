0a221a234de0f3b346a36105fa3780eef0e0647b
snwkng <dmpetlaj@gmail.com>
2026-04-10 02:44:58 +0300

    setTimeout memory leak fix

--- src/shared/lib/useDebounces.ts
+++ src/shared/lib/useDebounces.ts
@@ -2,8 +2,8 @@ export const debounce = (func: Function, delayMs: number = 1000) => {
   let timeout: ReturnType<typeof setTimeout>;
 
   return (...args: any[]) => {
-    clearTimeout(timeout);
     timeout = setTimeout(() => {
+      clearTimeout(timeout);
       func(...args);
     }, delayMs);
   };
